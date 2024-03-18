f70625a08b27aba91bc8b0b8871fcbbde779df9e
Lucas-selini <lucas.selini@ensta-paris.fr>
2024-03-18 19:10:39 +0100

    preparation of main.py for analysis

--- Projet/principal_DBN_alpha.py
+++ Projet/principal_DBN_alpha.py
@@ -4,20 +4,31 @@ from utils.utils import *
 # Paramètres liés au réseau et à l'apprentissage
 p = 320
 q = 50
-learning_rate = 0.25
+learning_rate = 0.2
 batch_size = 5
-nb_iter = 1000
+nb_iter = 100
 data_sample = [10] # 10 = A, B = 11, ..., Z = 35
 
 nb_gibbs_iteration = 20
-nb_image_generate = 5
+nb_image_generate = 1
 
 # Chargement des données
 data = lire_alpha_digit(data_sample)
 
-# Entrainement du DBN
-dbn_model = DBN([p,q,p+10,q+10,p+20,q+20,p+30])
-_ = dbn_model.train(data, nb_iter,learning_rate, batch_size)
+# # Entrainement du DBN
+# dbn_model = DBN([p,q,p+10,q+10,p+20,q+20,p+30])
+# _ = dbn_model.train(data, nb_iter,learning_rate, batch_size)
 
-# Génération d'images
-display_image(dbn_model.generer_image(nb_gibbs_iteration,nb_image_generate),20,16,save=True)
+# # Génération d'images
+# display_image(dbn_model.generer_image(nb_gibbs_iteration,nb_image_generate),20,16,save=True)
+
+# Etude de l'influence des différents hyperparamètres
+learning_rates = [0.001,0.01,0.1,0.2,0.3,0.4,0.5]
+images = []
+
+for learning_rate in learning_rates:
+    dbn_model = DBN([p,q,p+10,q+10,p+20,q+20,p+30])
+    _ = dbn_model.train(data, nb_iter,learning_rate, batch_size)
+    images.append(dbn_model.generer_image(nb_gibbs_iteration,nb_image_generate))
+    
+display_image(images,20,16,save=True)
--- Projet/principal_RBM_alpha.py
+++ Projet/principal_RBM_alpha.py
@@ -4,20 +4,31 @@ from utils.utils import *
 # Paramètres liés au réseau et à l'apprentissage
 p = 320
 q = 50
-learning_rate = 0.25
+learning_rate = 0.1
 batch_size = 5
-nb_iter = 1000
+nb_iter = 100
 data_sample = [10] # 10 = A, B = 11, ..., Z = 35
 
 nb_gibbs_iteration = 20
-nb_image_generate = 5
+nb_image_generate = 1
 
 # Chargement des données
 data = lire_alpha_digit(data_sample)
 
-# Entrainement du DBN
-rbm_model = RBM(p,q)
-_ = rbm_model.train(data, learning_rate, batch_size, nb_iter)
+# # Entrainement du DBN
+# rbm_model = RBM(p,q)
+# _ = rbm_model.train(data, learning_rate, batch_size, nb_iter)
 
-# Génération d'images
-display_image(rbm_model.generer_image(nb_gibbs_iteration,nb_image_generate),20,16,save=True)
+# # Génération d'images
+# display_image(rbm_model.generer_image(nb_gibbs_iteration,nb_image_generate),20,16,save=True)
+
+# Etude de l'influence des différents hyperparamètres
+learning_rates = [0.001,0.01,0.1,0.2,0.3,0.4,0.5]
+images = []
+
+for learning_rate in learning_rates:
+    rbm_model = RBM(p,q)
+    _ = rbm_model.train(data, learning_rate, batch_size, nb_iter)
+    images.append(rbm_model.generer_image(nb_gibbs_iteration,nb_image_generate))
+    
+display_image(images,20,16,save=True)
