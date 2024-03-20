b393753e7c1b73c037d31f0cc7e95dfd0f7ccbcb
Lucas-selini <lucas.selini@ensta-paris.fr>
2024-03-20 22:57:15 +0100

    this time its finalized

--- Projet/principal_DNN_MNIST_Fig1.py
+++ Projet/principal_DNN_MNIST_Fig1.py
@@ -19,13 +19,10 @@ import matplotlib.pyplot as plt
 def main():
     # Load the MNIST dataset for the specified digit classes
     nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    X, Y = lire_MNIST(nums)
-
-    # Split the dataset into training and testing sets
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1/7, random_state=42)
-
+    X_train, X_test, Y_train, Y_test = lire_MNIST(nums)
+    
     # Define the parameters for the DNN and the training process
-    p = X.shape[1]
+    p = X_train.shape[1]
     q = 200
     learning_rate = 0.075
     batch_size = 125
